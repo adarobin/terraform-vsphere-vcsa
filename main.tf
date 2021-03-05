19bf1805849999a0ba75d43dbcaa71b2049cc2e4
Adam Robinson <adam.e.robinson@icloud.com>
2021-03-05 09:42:25 -0500

    fix firmware and swap_placement_policy

--- main.tf
+++ main.tf
@@ -55,11 +55,11 @@ resource "vsphere_virtual_machine" "vcsa" {
   nested_hv_enabled      = data.vsphere_ovf_vm_template.ova.nested_hv_enabled
   memory                 = var.memory_override > 0 ? var.memory_override : data.vsphere_ovf_vm_template.ova.memory
   memory_hot_add_enabled = data.vsphere_ovf_vm_template.ova.memory_hot_add_enabled
-  swap_placement_policy  = data.vsphere_ovf_vm_template.ova.swap_placement_policy
   annotation             = data.vsphere_ovf_vm_template.ova.annotation
   guest_id               = data.vsphere_ovf_vm_template.ova.guest_id
   alternate_guest_name   = data.vsphere_ovf_vm_template.ova.alternate_guest_name
-  firmware               = data.vsphere_ovf_vm_template.ova.firmware
+  //swap_placement_policy  = data.vsphere_ovf_vm_template.ova.swap_placement_policy
+  //firmware               = data.vsphere_ovf_vm_template.ova.firmware
 
   scsi_type      = "lsilogic"
   enable_logging = true
